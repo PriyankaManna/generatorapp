0a9d1573ece195feb34f15bbe24f95e343188dcf
PriyankaManna <priyanka.manna@bacancy.com>
2022-05-19 14:40:32 +0530

    generator task

--- lib/generators/initializer_generator.rb
+++ lib/generators/initializer_generator.rb
@@ -1,9 +1,9 @@
 class InitializerGenerator < Rails::Generators::NamedBase
     source_root File.expand_path('templates', __dir__)
 
-    def create_initializer_file
-      create_file "config/initializers/initializer.rb", "# Add initialization content here"
-    end
+    # def create_initializer_file
+    #   create_file "config/initializers/initializer.rb", "# Add initialization content here"
+    # end
     
     def copy_initializer_file
         copy_file "initializer.rb", "config/initializers/#{file_name}.rb"
